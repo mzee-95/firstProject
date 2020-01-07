ec1f65bd49c7cc8d5d902f2241e12118f900b9e6
mzee-95 <59595024+mzee-95@users.noreply.github.com>
2020-01-07 10:41:42 +0200

    Update TeamCity_Bot.py

--- TeamCity_Bot.py
+++ TeamCity_Bot.py
@@ -11,13 +11,10 @@ bot_token = '928532602:AAEoedP50RLpJ_jQV3xi3PLVi8ibpFCGupE'
 
 bot = telebot.TeleBot(token=bot_token)
 
-#We squash bugs chat_id: -1001225534207
-#Test group chat_id: -375105540
 
 chat_id = -1001265292288
 
 bot.send_message(chat_id=chat_id, text='TC Build started')
 
 
-#Hercules: 10.3.1.154
-#Marno: 10.100.4.38
+
