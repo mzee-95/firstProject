b86d6da215d6ca1939f1f62b0dea4af390aa7748
Marno Grobler <njmgrobler@gmail.com>
2020-01-07 14:01:40 +0200

    Update TeamCity_Bot.py

--- TeamCity_Bot.py
+++ TeamCity_Bot.py
@@ -2,11 +2,13 @@ import telebot
 import os
 import sys
 
-bot_token = '928532602:AAEoedP50RLpJ_jQV3xi3PLVi8ibpFCGupE'
+#bot_token = '928532602:AAEoedP50RLpJ_jQV3xi3PLVi8ibpFCGupE'
 
-bot = telebot.TeleBot(token=bot_token)
+#bot = telebot.TeleBot(token=bot_token)
 
 chat_id = -1001265292288
+
+
 
 if 'Started' in sys.argv:
     message = 'Build Started'
@@ -14,4 +16,5 @@ if 'Started' in sys.argv:
 if 'Finished' in sys.argv:
     message = 'Build Successful'
 
-bot.send_message(chat_id=chat_id, text=message)
+#bot.send_message(chat_id=chat_id, text=message)
+sleep(30)
