400fb13ea2cd18e1fc9f67e154b46d873f981269
mzee-95 <59595024+mzee-95@users.noreply.github.com>
2020-01-07 09:35:27 +0200

    Update Program.cs

--- ConsoleApplication3/ConsoleApplication3/Program.cs
+++ ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApplication3
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello Marno...");
+            Console.ReadKey();
         }
     }
 }
